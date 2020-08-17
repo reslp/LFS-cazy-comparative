aa42b549a3f51daa0eec59b40ede49a18f5c7f63
Philipp Resl <resl.philipp@gmail.com>
2020-08-17 16:00:17 +0200

    Add serial option and manual handling of submission system in script

--- submit.sh
+++ submit.sh
@@ -8,6 +8,7 @@ usage() {
 	echo "Usage: $0 [-v] [-c <cluster_config_file>] [-s <snakemke_args>]" 
 	echo
 	echo "Options:"
+	echo "  -t <submission_system> Specify available submission system. Options: sge, slurm, serial (no submission system). Default: Automatic detection."
 	echo "	-c <cluster_config_file> Path to cluster config file in YAML format (mandatory). "
 	echo "	-s \"<snakemake_args>\" Additional arguments passed on to the snakemake command (optional). snakemake is run with --immediate-submit -pr --notemp --latency-wait 600 --use-singularity --jobs 1001 by default." 
 	echo "	-i \"<singularity_args>\" Additional arguments passed on to singularity (optional). Singularity is run with -B /tmp:/usertmp by default."
@@ -18,11 +19,13 @@ version() {
 	exit 0
 }
 
-while getopts ":v:c:s:i:" option;
+CLUSTER=""
+while getopts ":v:t:c:s:i:" option;
 	do
 		case "${option}"
 		in
 			v) version;;
+			t) CLUSTER=${OPTARG};;
 			c) CLUSTER_CONFIG=${OPTARG};;
 			s) SM_ARGS=${OPTARG};;
 			i) SI_ARGS=${OPTARG};;
@@ -32,10 +35,27 @@ while getopts ":v:c:s:i:" option;
 	done
 if [ $OPTIND -eq 1 ]; then usage; fi
 
-CLUSTER=""
-command -v qsub >/dev/null 2>&1 && { echo >&2 "SGE detected, will use qsub to submit jobs."; CLUSTER="sge"; }
-command -v sbatch >/dev/null 2>&1 && { echo >&2 "SLURM detected, will use sbatch to submit jobs."; CLUSTER="slurm"; }
-echo $SI_ARGS
+# Determine submission system:
+if [[ $CLUSTER == "sge" ]]; then
+	echo "SGE (Sun Grid Engine) submission system specified. Will use qsub to submit jobs."
+elif [[ $CLUSTER == "slurm" ]]; then
+	echo "SLURM submission system specified. Will use sbatch to submit jobs."
+elif [[ $CLUSTER == "serial" ]]; then
+  echo "Serial execution without job submission specified."
+else
+	echo "No or unknown submission system specified, will try to detect the system automatically."
+	CLUSTER=""
+	command -v qsub >/dev/null 2>&1 && { echo >&2 "SGE detected, will use qsub to submit jobs."; CLUSTER="sge"; }
+	command -v sbatch >/dev/null 2>&1 && { echo >&2 "SLURM detected, will use sbatch to submit jobs."; CLUSTER="slurm"; }
+  if [[ $CLUSTER == "" ]]; then
+    echo "Submission system could not be detected. You may be able to run the pipeline without job submission."
+    exit 1
+  fi
+fi
+
+echo "Additional arguments passed on to singularity: $SI_ARGS"
+echo "Additional arguments passed on to snakemake: $SM_ARGS"
+
 if [ $CLUSTER = "slurm" ]; then
 	export CONDA_PKGS_DIRS="$(pwd)/.conda_pkg_tmp"
 	mkdir -p .conda_pkg_tmp
@@ -43,6 +63,8 @@ if [ $CLUSTER = "slurm" ]; then
 	unset CONDA_PKGS_DIRS
 elif [ $CLUSTER = "sge" ]; then
 	snakemake --use-conda --use-singularity --singularity-args "-B $(pwd)/external/deeploc-1.0/bin/:/external -B $(pwd)/external/deeploc-1.0/DeepLoc:/usr/lib/python3/dist-packages/DeepLoc -B $(pwd):/data -B $(pwd)/bin:/usr/local/external -B /tmp:/usertmp $SI_ARGS" --jobs 1001 --cluster-config $CLUSTER_CONFIG --cluster "$(pwd)/bin/immediate_submit.py '{dependencies}' sge" --immediate-submit -pr --notemp --latency-wait 600 $SM_ARGS
+elif [ $CLUSTER = "serial" ]; then
+	snakemake --use-conda --use-singularity --singularity-args "-B $(pwd)/external/deeploc-1.0/bin/:/external -B $(pwd)/external/deeploc-1.0/DeepLoc:/usr/lib/python3/dist-packages/DeepLoc -B $(pwd):/data -B $(pwd)/bin:/usr/local/external -B /tmp:/usertmp $SI_ARGS" -pr --notemp --latency-wait 600 $SM_ARGS
 else
 	echo "Submission system not recognized"
 	exit 1
