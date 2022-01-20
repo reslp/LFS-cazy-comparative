b1e6930911aeae5296e841728c08084a77b72497
Philipp Resl <resl.philipp@gmail.com>
2022-01-20 09:29:07 +0100

    setup: add copying of phylociraptor input files

--- setup.sh
+++ setup.sh
@@ -9,10 +9,12 @@ echo "Part 2: Gene calling and functional annotation"
 echo "Part 3: Comparative genomic analysis"
 echo
 echo "The analyses performed in each part have to be run individually, look at the instructions in the README file."
+echo "You may see git detached HEAD warning during this setup, these warnings can be ignored."
 read -p "Would you like to setup the analysis now? (y/n)" proceed
 
 if [[ $proceed == "y"  || $proceed == "Y" ]]
 then
+	echo "------------------------------------------------------------"
 	echo "Will setup Part1 (phylogenomics)..."
 	echo "Will clone phylociraptor into the ./phylogeny directory"
 	git clone --recursive https://github.com/reslp/phylociraptor.git phylogeny/phylociraptor
@@ -21,7 +23,14 @@ then
 	git checkout a93b4c8
 	# add genome download here once we have NCBI accession numbers
 	cd ../..
-		
+	echo "Will copy over config files for phylociraptor from input-data/phylociraptor."
+	echo "These files contain settings and taxon selection used in the study."
+	echo "HINT: Currently these files do not contain the final NCBI accession number as we are still waiting for them..."
+	cp input-data/phylciraptor/config.yaml phylogeny/phylociraptor/data
+	cp input-data/phylociraptor/LFS-phylogenomics-taxon-selection.csv phylogeny/phylociraptor/data	
+	echo "Files copied successfully. You may now look at the README file to see how to recreate phylogenomic results."
+	
+	echo "------------------------------------------------------------"
 	echo "Will setup Part2 (genome annotation)..."
 	echo "Will clone smsi-funannotate into the genome-annotation directory"
 	git clone --recursive https://github.com/reslp/smsi-funannotate.git genome-annotation/smsi-funannotate 
